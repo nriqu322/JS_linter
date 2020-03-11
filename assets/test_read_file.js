d790bee2e29be3f9a41f56be7fda1ed3fa3d4124
Luis Saavedra <nriqu322@gmail.com>
2020-03-11 15:04:42 -0600

    added tests to a .js file in assets

--- assets/test_read_file.js
+++ assets/test_read_file.js
@@ -1,4 +1,12 @@
-firstName = "John";
-lastName = "Doe";
+firstName = 'John';
+lastName = 'Doe';
 
 fullName = firstName + lastName;
+
+function isEven(num) {
+  return num % 2 == 0;
+};
+
+function sum(x, y){
+  return x + y;
+};
