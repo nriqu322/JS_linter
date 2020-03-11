196c4713a25c443041242422719e1afe90205b82
Luis Saavedra <nriqu322@gmail.com>
2020-03-11 15:06:14 -0600

    readfile class modified to output a string of every line

--- lib/read_file.rb
+++ lib/read_file.rb
@@ -1,6 +1,15 @@
 class ReadFile
   def get_file_data(path)
-    file_data = File.read(path).split
-    file_data
+    #line_num = 0
+    #array = []
+    #File.foreach(path) { |line| array << "#{line}" } 
+
+    #file_data = File.read(path).split
+    #file_data
+
+    file = File.open(path)
+    content = file.readlines.map(&:chomp)
+    file.close
+    content
   end
 end
