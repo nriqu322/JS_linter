76e17d31fdfb52ccdc60694aa5ea6e539fb87db9
Luis Saavedra <nriqu322@gmail.com>
2020-03-11 18:40:02 -0600

    added semicolon check at the end of line

--- assets/test_read_file.js
+++ assets/test_read_file.js
@@ -3,10 +3,14 @@ lastName = 'Doe';
 
 fullName = firstName + lastName;
 
+var x = 5;
+var y = 7;
+console.log(x + y)
+
 function isEven(num) {
   return num % 2 == 0;
-};
+}
 
 function sum(x, y) {
   return x + y;
-};
+}
--- lib/js_parse.rb
+++ lib/js_parse.rb
@@ -15,9 +15,11 @@ class JsParse
     end
   end
 
-  # def end_semicolon(line)
-  #   if .match(line)
-  # end
+  def end_semicolon(line, line_num)
+    if /^[^\n|\}|function](?:(?!;).)*$/.match(line)
+      puts 'ERROR: '.red + "line #{line_num}, missing semicolon at the end of line"
+    end
+  end
 
   def space_before_braces(line, line_num)
     if /\S\{/.match(line)
@@ -27,18 +29,18 @@ class JsParse
 
   def space_end_line(line, line_num)
     if /[\w]\s\s/.match(line)
-      puts  'ERROR: '.red + "line #{line_num}, check for double spaces."
+      puts 'ERROR: '.red + "line #{line_num}, check for double spaces."
     elsif /\s$/.match(line)
-      puts  'ERROR: '.red + "line #{line_num}, check for double spaces at the end of the line."
+      puts 'ERROR: '.red + "line #{line_num}, remove spaces at the end of the line."
     end
   end
 
   def check_braces
     diff = @open_brace - @close_brace
     if diff < 0
-      puts "ERROR: ".red + "Number of missing open braces : #{diff}."
+      puts 'ERROR: '.red + "Number of missing open braces : #{diff}."
     elsif diff > 0
-      puts "ERROR: ".red + "Number of missing closed braces : #{diff}."
+      puts 'ERROR: '.red + "Number of missing closed braces : #{diff}."
     end
   end
 
@@ -62,6 +64,7 @@ class JsParse
       space_before_braces(line, i + 1)
       count_pair_braces?(line, i + 1)
       space_end_line(line, i + 1)
+      end_semicolon(line, i +1)
     end
     check_braces
   end
